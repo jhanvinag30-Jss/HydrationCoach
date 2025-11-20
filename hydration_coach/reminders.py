e6c36131c90be089edd0f8bbe3e92fdaca456057
jhanvinag30-Jss <jhanvinag30@gmail.com>
2025-11-20 22:40:18 +0530

    Update reminders.py

--- hydration_coach/reminders.py
+++ hydration_coach/reminders.py
@@ -1,1 +1,5 @@
-
+def get_hourly_reminders(start_hour,end_hour):
+  reminders=() #list
+  for hour in range(start/-hour,end_hour+1):
+    reminders.append(f"Remember:Drink water at {hour}:00")
+    return reminders
