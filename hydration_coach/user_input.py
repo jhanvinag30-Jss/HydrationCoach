059ae3822f3d9d47997d6e0a4d7fc0a1e4baed59
jhanvinag30-Jss <jhanvinag30@gmail.com>
2025-11-20 22:45:07 +0530

    Update user_input.py

--- hydration_coach/user_input.py
+++ hydration_coach/user_input.py
@@ -1,1 +1,16 @@
+# Handles input operations and basic validation using loops and conditionals.
+def get_weight_from_user():
+  while True:
+    user_input=input("Enter your weight in kg:")
+    if user_input.isdigit():
+      return int(user_input)
+    else:
+      print("Please enter a valid number.")
 
+def get_today_intake():
+  while True:
+    user_input=input("How much water have you drink today (ml)?")
+    if user_input,isdigits():
+       return int(user_input)
+else: 
+   print("Enter a valid number.")
