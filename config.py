fda79765d1d1d110954ea914672563d30c8b746d
jhanvinag30-Jss <jhanvinag30@gmail.com>
2025-11-21 22:11:44 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-"""
-Configuration values for HydrationCoach.
-"""
-
-MIN_WATER_ML = 1500        # Minimum recommended water (ml)
-ML_PER_KG = 35             # Water requirement per kg
-DEFAULT_INTERVAL_ML = 250  # Default reminder interval
